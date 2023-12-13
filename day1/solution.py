27ab55ea0a416c615e1b23202c0d1ff542c5b417
Yiyi Yang <yiyi.yang3@mail.mcgill.ca>
2023-12-13 21:30:08 +0800

    addd day1 part2

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as f:
     data = f.read().splitlines()
@@ -11,3 +12,25 @@ for l in data:
             num += c
     res += int(num[0]+num[-1])
 print(res)
+
+
+with open(os.path.join(os.path.dirname(__file__),"input.txt")) as f:
+    data = f.read().splitlines()
+
+res2=0
+dict = {"one":1,"two":2,"three":3,
+        "four":4,"five":5,"six":6,
+        "seven":7,"eight":8,"nine":9}
+for s in data:
+    num = ""
+    k=0
+    for i in range(len(s)):
+        for word in dict.keys():
+            if word in s[k:i+1]:
+                num+=str(dict[word])
+                k=i-1
+        if s[i].isnumeric():
+            num+=s[i]
+
+    res2 += int(num[0]+num[-1])
+print(res2)
