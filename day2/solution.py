f0c6561cf2b7baad78c2c976f2dc56f544bb1cce
Yiyi Yang <yiyi.yang3@mail.mcgill.ca>
2023-12-16 16:49:22 +0800

    add part2
     day2

--- day2/solution.py
+++ day2/solution.py
@@ -1,29 +1,49 @@
 import os
-import re
+import functools
 
 with open(os.path.join(os.path.dirname(__file__),'input.txt')) as f:
     data = f.read().splitlines()
 
-res = 0
-for l in data:
-    if l in ["\n",""]:
-        continue
-    print(l)
-    x,plays = l.split(": ")
-    dict={"red":12,"blue":14,"green":13}
+def part1(data):
+    res = 0
+    for l in data:
+        if l in ["\n",""]:
+            continue
+        x,plays = l.split(": ")
+        dict={"red":12,"blue":14,"green":13}
 
-    game_id = int(x.replace("Game ",""))
-    plays = plays.split("; ")
-    flag = 0
-    for p in plays:
-        draws = p.split(", ")
-        for d in draws:
-            num, color = d.split(" ")
-            if dict[color]<int(num):
-                flag=1
-    if not flag:
-        res+=game_id
-    else:
-        print("game_id",game_id)
+        game_id = int(x.replace("Game ",""))
+        plays = plays.split("; ")
+        flag = 0
+        for p in plays:
+            draws = p.split(", ")
+            for d in draws:
+                num, color = d.split(" ")
+                if dict[color]<int(num):
+                    flag=1
+        if not flag:
+            res+=game_id
 
-print(res)
+    print(res)
+
+
+def part2(data):
+    res = 0
+    for l in data:
+        if l in ["\n",""]:
+            continue
+        x,plays = l.split(": ")
+        dict={"red":0,"blue":0,"green":0}
+
+        plays = plays.split("; ")
+        for p in plays:
+            draws = p.split(", ")
+            for d in draws:
+                num, color = d.split(" ")
+                if dict[color]<int(num):
+                    dict[color] = int(num)
+        res+=functools.reduce(lambda x,y:x*y,dict.values())
+    print(res)
+
+part1(data)
+part2(data)
